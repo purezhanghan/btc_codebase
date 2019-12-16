08604b6cfb8c5906644a06816b9f5a577f3366b2
Entheos1994 <pete.zhanghan@yahoo.com>
2019-12-16 20:29:57 +0800

    add eval mode & logging module

--- README.md
+++ README.md
@@ -3,25 +3,51 @@ A code base for testing btc trade algorithm
 
 ---
 ##### train mode:<br>
-train with holding_amount_data and btc price, model selected from **LinearRegression**, **SVR**, **KNeighborsRegressor**
+Model trained with holding_amount_data and btc price, algorithm selected from **LinearRegression**, **SVR**, **KNeighborsRegressor**
 ```
 python btc_model.py \
 	--grid_search False \
 	--read_path './data' \
 	--save_path './output' \
 	--train
+
+# given "btc_price, 持有量100-100, etc.." in data folder
+# return best model and store in output folder
+
+return example:
+
+LinearRegression       R-squared: -10.418
+best_score: -10.418
+SVR                    R-squared: -4.444
+best_score: -4.444
+KNeighborsRegressor    R-squared: 0.517
+best_score: 0.517
+trend simultaneous acc: 0.979
+save model in paht KNeighborsRegressor
+
 ```
 
 ##### eval mode:
-eval model without training, assuming there is model existed in *output* directory
+eval model without training, assuming there is model existed in *output* directory. Prediction will be appened to *data/pred.csv*.
 ```
 python btc_model.py \
-	-- save_path './output' \
-	--train False
+	--read_path './data' \
+	--save_path './output' \
+	--t_next
+# given "持有量100-1000-test, etc.." in data folder, test tag must be included in file name
+# return T+1/T+N prediction(make sure there is a model get trained before)
+
+T+1 example:
+current time: 2019-11-27 07:37:04
+next time: 2019-11-27 10:37:04
+price prediction: 7771.996548204
+
 ```
 #### Todo:
-- [ ] Eval Mode: test trained model, return prediction with given data
+- [x] Eval Mode: test trained model, return prediction with given data [2019.12.16]
 - [ ] Plotting: draw price and prediction in real time
 - [ ] More auxiliary data: e.g. mood data
 - [ ] Model's Parameter Search: random search, bayesian
 - [ ] Advanced Model: RNN coming
+- [ ] Data quality checking
+- [x] Use logging to replace print
--- btc_model.py
+++ btc_model.py
@@ -1,8 +1,9 @@
 # data
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
+import sys
 
 # models
 from sklearn.dummy import DummyRegressor
@@ -24,17 +25,26 @@ from sklearn.preprocessing import MinMaxScaler
 
 # warnings
 import warnings
+
+# pickle
+import pickle
+
+# logging
+import logging.config
+import logging
+
+# yml
+import yaml
+
+
 warnings.simplefilter('ignore')
 
 PRICE_NAME = ['价格']
 HOLD_AMOUNT_NAME = ['100-1000', '1000-10000', '10000-100000']
 # NORM_COLS = ['价格', '100-1000', '1000-10000', '10000-100000']
-NORM_COLS = PRICE_NAME + HOLD_AMOUNT_NAME
 SPLIT_SIZE = 0.7
 
 MODEL_LIST = [LinearRegression, SVR, KNeighborsRegressor]
-
-SCALER_DICT = OrderedDict()
 
 
 def _sep_detect(path):
@@ -67,17 +77,17 @@ def hyper_parameter_search():
     pass
 
 
-def _parse_path(base_path, keyword):
+def _parse_path(base_path, keyword, train):
     bool_list = [keyword in s for s in os.listdir(base_path)]
     if any(bool_list):
         sub_path = os.listdir(base_path)[bool_list.index(True)]
         read_path = os.path.join(base_path, sub_path)
         return read_path
     else:
-        if keyword == PRICE_NAME[0]:
-            raise ValueError('price data path is invalid.')
+        if keyword == PRICE_NAME[0] and not train:
+            raise ValueError('price data path is invalid, exit')
         else:
-            print('Path for auxiliary data {} is invalid'.format(keyword))
+            logger.warn('Path for auxiliary data {} is invalid\n'.format(keyword))
         return None
 
 
@@ -87,14 +97,14 @@ def _data_clean(key, data):
         try:
             data = data[data[col].apply(lambda x: x.isnumeric())]
         except AttributeError:
-            print('{} has no string values in column {}'.format(key, col))
+            logger.debug('{} has no string values in column {}\n'.format(key, col))
 
     data['timestamp'] = data['timestamp'].astype(int)
     data[key] = data[key].astype(float)
     data.loc[:, '{}_datetime'.format(key)] = data.apply(
         lambda row: pd.to_datetime(datetime.fromtimestamp(row['timestamp'])), axis=1)
-    print('{} \nmax datetime: {} \nmin datetime {}\n'.format(
-        key, data['{}_datetime'.format(key)].max(), data['{}_datetime'.format(key)].min()))
+    logger.info('{} max datetime: {}'.format(key, data['{}_datetime'.format(key)].max()))
+    logger.info('{} min datetime: {}\n'.format(key, data['{}_datetime'.format(key)].min()))
     data = data.sort_values(['{}_datetime'.format(key)])
     return data
 
@@ -111,18 +121,33 @@ def _find_nearest_pos(current_t, aux_data, aux_key):
 def _merge_aux_data(merged_data, aux_data, aux_key):
     merged_data.loc[:, '{}_time_nearest'.format(aux_key)] = merged_data.apply(
         lambda row: _find_nearest_pos(row['timestamp'], aux_data, aux_key), axis=1)
+    merged_data = merged_data.loc[merged_data['{}_time_nearest'.format(
+        aux_key)] != 'exceed condition']
+    merged_data['{}_time_nearest'.format(aux_key)] = pd.to_datetime(
+        merged_data['{}_time_nearest'.format(aux_key)])
+    aux_data['{}_datetime'.format(aux_key)] = pd.to_datetime(
+        aux_data['{}_datetime'.format(aux_key)])
     merged_data = merged_data.merge(aux_data.drop('timestamp', axis=1), left_on=[
         '{}_time_nearest'.format(aux_key)], right_on=['{}_datetime'.format(aux_key)])
     return merged_data
 
 
-def _normalize(df):
-    global SCALER_DICT
-    for col in NORM_COLS:
-        scaler = MinMaxScaler()
-        df['{}_norm'.format(col)] = scaler.fit_transform(
-            df[col].values.reshape(-1, 1))
-        SCALER_DICT[col] = scaler
+def _normalize(df, scaler_dict, is_training):
+    if is_training:
+        for col in PRICE_NAME + HOLD_AMOUNT_NAME:
+            scaler = MinMaxScaler()
+            df['{}_norm'.format(col)] = scaler.fit_transform(
+                df[col].values.reshape(-1, 1))
+            scaler_dict[col] = scaler
+    else:
+        for col in HOLD_AMOUNT_NAME:
+            try:
+                df['{}-test_norm'.format(col)] = scaler_dict[col].transform(
+                    df['{}-test'.format(col)].values.reshape(-1, 1))
+            except ValueError:
+                logger.error(
+                    f"There is no scaler prepared for {col} during test process, exit system\n")
+                sys.exit(1)
     return df
 
 
@@ -148,15 +173,15 @@ def _model_tune(data, norm=True):
             # best_train_pred = train_pred
             best_test_pred = test_pred
             best_model = reg
-        print(
+        logger.info(
             f"{model.__name__:22} R-squared: "
-            f"{cur_score:.3f}\n"
+            f"{cur_score:.3f}\t"
             f"best_score: {best_score:.3f}"
         )
     return best_model, best_test_pred, y_test
 
 
-def _validation(pred, target, scaler_dict):
+def _validation(pred, target):
     val_df = pd.DataFrame(data=np.hstack(
         (pred.reshape(-1, 1), target.values.reshape(-1, 1))), columns=['pred', 'target'])
     val_df['target_shift'] = val_df['target'].shift(-1)
@@ -167,16 +192,18 @@ def _validation(pred, target, scaler_dict):
     val_df['simult'] = val_df.apply(
         lambda row: row['target_trend'] * row['pred_trend'] >= 0, axis=1)
 
-    print(
+    logger.info(
         f"trend simultaneous acc: {val_df['simult'].sum() / len(val_df):.3f}"
     )
 
 
 def train_main(args):
+    # scaler_dict
+    scaler_dict = OrderedDict()
     # read in data
     data_dict = OrderedDict()
     for keyword in PRICE_NAME + HOLD_AMOUNT_NAME:
-        read_in_path = _parse_path(args.read_path, keyword)
+        read_in_path = _parse_path(args.read_path, keyword, args.train)
         if read_in_path is not None:
             cleaned_data = _data_clean(keyword, _read_data(read_in_path))
             # drop idnex necessary!
@@ -192,29 +219,106 @@ def train_main(args):
             merged_data, aux_data, aux_key)
 
     # normalization
-    norm_data = _normalize(merged_data)
+    norm_data = _normalize(merged_data, scaler_dict, args.train)
 
     # model
     best_model, best_test_pred, y_test = _model_tune(norm_data)
 
     # validation
 
-    _validation(best_test_pred, y_test, SCALER_DICT)
+    _validation(best_test_pred, y_test)
+
+    # if save
+    save_path = args.save_path + '/' + \
+        str(datetime.today().replace(microsecond=0)) + '_saved_model_v1.pkl'
+    character_map = {
+        ord(':'): '_',
+        ord('-'): '_',
+        ord(' '): '_'
+    }
+    save_path = save_path.translate(character_map)
+    save_dict = {'scaler': scaler_dict, 'model': best_model}
+    with open(save_path, 'wb') as f:
+        pickle.dump(save_dict, f)
+        logger.info(f"save model in path {type(best_model).__name__}")
 
 
 # to do
 def test_main(args):
-    pass
+    model_path = max(os.listdir(args.save_path))
+    try:
+        with open(os.path.join(args.save_path, model_path), 'rb') as f:
+            read_dict = pickle.load(f)
+    except pickle.UnpicklingError:
+        logger.error('model cannot be loaded, check if there is any non-pickle exited in data folder.')
+        sys.exit(1)
+
+    model = read_dict['model']
+    scaler_dict = read_dict['scaler']
+
+    data_dict = OrderedDict()
+    for keyword in HOLD_AMOUNT_NAME:
+        keyword = keyword + '-test'
+        read_in_path = _parse_path(args.read_path, keyword, args.train)
+        if read_in_path is not None:
+            cleaned_data = _data_clean(keyword, _read_data(read_in_path))
+            # drop idnex necessary!
+            cleaned_data = cleaned_data.reset_index(drop=True)
+            data_dict[keyword] = cleaned_data
+
+    # align to 100-1000 as main timeline
+    merged_data = data_dict[HOLD_AMOUNT_NAME[0] + '-test']
+    for aux_key, aux_data in data_dict.items():
+        if aux_key == HOLD_AMOUNT_NAME[0] + '-test':
+            continue
+        merged_data = _merge_aux_data(
+            merged_data, aux_data, aux_key)
+
+    # normalization
+    norm_data = _normalize(merged_data, scaler_dict, args.train)
+
+    # prediction
+    norm_list = [i for i in norm_data.columns if 'norm' in i]
+    pred = model.predict(norm_data[norm_list])
+    pred_inverse = scaler_dict[PRICE_NAME[0]
+                               ].inverse_transform(pred.reshape(-1, 1))
+    pred_df = norm_data[['{}-test_datetime'.format(HOLD_AMOUNT_NAME[0])]]
+    pred_df['pred_price'] = pred_inverse.flatten()
+    # hold amount time + 3h
+    pred_df.loc[:, 'price_datetime'] = pred_df.apply(
+        lambda row: row['{}-test_datetime'.format(HOLD_AMOUNT_NAME[0])] + timedelta(hours=3), axis=1)
+    pred_df.columns = ['t_datetime', 't1_price', 't1_datetime']
+    pred_df = pred_df.sort_values('t_datetime')
+    pred_df['pred_time'] = datetime.today().replace(microsecond=0)
+    if args.t_next:
+        logger.info(
+            f'current time: {pred_df.iloc[-1]["t_datetime"]}\nnext time: {pred_df.iloc[-1]["t1_datetime"]}\nprice prediction: {pred_df.iloc[-1]["t1_price"]}')
+        pred_df.iloc[-1].to_csv(args.read_path + '/pred.csv', mode='a')
+    else:
+        pred_df.to_csv(args.read_path + '/pred.csv', mode='a')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--grid_search", default=False,
                         help='Turn on grid search on given parameters.', action='store_true')
-    parser.add_argument("--read_path", default='./', help='default read path')
-    parser.add_argument("--save_path", default='./', help='default save path')
-    parser.add_argument("--train", help='train or eval mode', default=True)
+    parser.add_argument("--read_path", default='./',
+                        help='default read path')
+    parser.add_argument("--save_path", default='./',
+                        help='default save path')
+    parser.add_argument(
+        "--train", help='train or eval mode', action='store_true')
+    parser.add_argument(
+        "--t_next", help='predict next time price only', action='store_true')
     args = parser.parse_args()
+
+    # load logger config
+    with open('./logging.yml', 'rt') as f:
+        config = yaml.safe_load(f.read())
+        f.close()
+    logging.config.dictConfig(config)
+    logger = logging.getLogger(__name__)
+    logger.info("Contest is starting")
     if args.train:
         train_main(args)
     else:
